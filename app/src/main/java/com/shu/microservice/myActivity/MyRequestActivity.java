a387a46d39844827fa5cb062b6bff38f1d7d88c4
Yage <13156525583@163.com>
2016-02-26 22:06:59 +0800

    完成“我的需求”、“我的服务”、“个人设置”设计。

--- app/src/main/java/com/shu/microservice/myActivity/MyRequestActivity.java
+++ app/src/main/java/com/shu/microservice/myActivity/MyRequestActivity.java
@@ -2,7 +2,11 @@ package com.shu.microservice.myActivity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.ListView;
+
 
 import com.shu.microservice.R;
 
@@ -10,12 +14,39 @@ import com.shu.microservice.R;
  * Created by Administrator on 2016/2/24 0024.
  */
 public class MyRequestActivity extends AppCompatActivity {
+    ListView listview = null;
+    ImageView imageView_back = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.my_request);
-        TextView tv1= (TextView) findViewById(R.id.request);
-        tv1.setText("this is test!,my request");
+
+        listview=(ListView)findViewById(R.id.request_list);
+        MyRequestList adapter=new MyRequestList(this);
+        listview.setAdapter(adapter);
+        listview.setItemsCanFocus(false);
+        listview.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                MyRequestList.ViewHolder vHollder = (MyRequestList.ViewHolder) view.getTag();
+                //在每次获取点击的item时将对于的checkbox状态改变，同时修改map的值。
+                vHollder.cBox.toggle();
+                MyRequestList.isSelected.put(position, vHollder.cBox.isChecked());
+            }
+        });
+
+        //返回到上一级页面
+        imageView_back = (ImageView) findViewById(R.id.back_button);
+        imageView_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
     }
 
 }
--- app/src/main/java/com/shu/microservice/myActivity/MySettingActivity.java
+++ app/src/main/java/com/shu/microservice/myActivity/MySettingActivity.java
@@ -1,21 +1,32 @@
 package com.shu.microservice.myActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.ImageView;
 
+
+import com.shu.microservice.MainActivity;
 import com.shu.microservice.R;
 
 /**
  * Created by Administrator on 2016/2/24 0024.
  */
 public class MySettingActivity extends AppCompatActivity {
+    ImageView imageView_back = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.my_setting);
-        TextView tv1= (TextView) findViewById(R.id.setting);
-        tv1.setText("this is test!,my setting");
+        //返回到上一级页面
+        imageView_back = (ImageView) findViewById(R.id.back_button);
+        imageView_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 
 }
