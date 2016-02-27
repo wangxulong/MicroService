f5d4a64c16841657bd5a6eea172c7ded6cc1e0c6
wangxulong <duanxian0402@126.com>
2016-02-27 09:58:07 +0800

    动态获取热点问答

--- app/src/main/java/com/shu/microservice/activity/QuestionFragment.java
+++ app/src/main/java/com/shu/microservice/activity/QuestionFragment.java
@@ -1,22 +1,37 @@
 package com.shu.microservice.activity;
 
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.shu.microservice.MainActivity;
 import com.shu.microservice.R;
 import com.shu.microservice.activity.question.QuestionDetail;
 import com.shu.microservice.adapter.QuestionAdapter;
 import com.shu.microservice.model.QuestionItem;
 import com.shu.microservice.util.AppContext;
+import com.shu.microservice.util.NetUtil;
 import com.shu.microservice.util.ToastUtil;
+import android.app.ProgressDialog;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -26,12 +41,16 @@ import java.util.List;
  * Created by wxl on 2016/2/22.
  */
 public class QuestionFragment extends Fragment {
+    public  static final String hotQuestionURL = "http://115.159.104.74:8080/micro_admin/ajax/hotQuestion";
     private ListView hotQuestion;
     private List<QuestionItem> hotQuestionItems;
     private QuestionAdapter adapter;
 
     private ListView laterUpdateQuestion;
     private List<QuestionItem> laterUpdateItems;
+
+    private  RequestQueue mRequestQueue;
+    private JsonObjectRequest mJsonObjectRequest;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -73,24 +92,60 @@ public class QuestionFragment extends Fragment {
         hotQuestion = (ListView) view.findViewById(R.id.question_hot);
         laterUpdateQuestion = (ListView) view.findViewById(R.id.question_later_update);
         initData();
-        adapter = new QuestionAdapter(hotQuestionItems);
-        hotQuestion.setAdapter(adapter);
+
 
         adapter = new QuestionAdapter(laterUpdateItems);
         laterUpdateQuestion.setAdapter(adapter);
 
     }
-
-    private void initData() {
-        hotQuestionItems = new ArrayList<>();
-        for(int i=0;i<3;i++){
-            QuestionItem item = new QuestionItem();
-            item.setId((long)i);
-            item.setAuthor("wxl" + i);
-            item.setTitle("自行车" + i);
-            item.setCreateTime(new Date());
-            hotQuestionItems.add(item);
+    private void getHotQuestion() {
+        if(!NetUtil.isConnected(AppContext.getAppContext())){
+            ToastUtil.showLong("网络连接失败");
+            return;
         }
+        // 1 创建RequestQueue对象
+        mRequestQueue = AppContext.getAppQueue();
+        // 2 创建JsonObjectRequest对象
+        mJsonObjectRequest = new JsonObjectRequest(hotQuestionURL, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        //
+                        try {
+                            if(response.getString("status").equals("success")){
+                                hotQuestionItems = new ArrayList<>();
+                                JSONArray results = response.getJSONArray("data");
+                                JSONObject result = null;
+                                for(int i=0;i<results.length();i++){
+                                    result = results.getJSONObject(i);
+                                    QuestionItem item = new QuestionItem();
+                                    item.setId(result.getLong("id"));
+                                    item.setAuthor(result.getString("userName"));
+                                    item.setTitle(result.getString("title"));
+                                    item.setCreateTime(new Date(result.getLong("createTime")));
+                                    hotQuestionItems.add(item);
+                                }
+                                adapter = new QuestionAdapter(hotQuestionItems);
+                                hotQuestion.setAdapter(adapter);
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        System.out.println("请求结果:" + response.toString());
+                    }
+
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println("请求错误:" + error.toString());
+            }
+        });
+        mJsonObjectRequest.setTag("hotQuestion");
+        // 3 将JsonObjectRequest添加到RequestQueue
+        mRequestQueue.add(mJsonObjectRequest);
+    }
+    private void initData() {
+        getHotQuestion();
         laterUpdateItems = new ArrayList<>();
         for(int i=0;i<10;i++){
             QuestionItem item = new QuestionItem();
@@ -103,4 +158,9 @@ public class QuestionFragment extends Fragment {
 
     }
 
+    @Override
+    public void onStop() {
+        super.onStop();
+        AppContext.getAppQueue().cancelAll("hotQuestion");
+    }
 }
--- app/src/main/java/com/shu/microservice/activity/question/QuestionDetail.java
+++ app/src/main/java/com/shu/microservice/activity/question/QuestionDetail.java
@@ -1,7 +1,11 @@
 package com.shu.microservice.activity.question;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.shu.microservice.R;
 import com.shu.microservice.util.ToastUtil;
@@ -10,11 +14,39 @@ import com.shu.microservice.util.ToastUtil;
  * Created by wxl on 2016/2/25.
  */
 public class QuestionDetail extends AppCompatActivity {
+    private TextView commentIcon;
+    private TextView backIcon;
+
+    private Long currentQuestionId;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.question_detail);
-        Long id=  getIntent().getLongExtra("id",-1);
-        ToastUtil.showShort(id+"");
+        Long currentQuestionId=  getIntent().getLongExtra("id",-1);
+        initViews();
+        initIcons();
+        initEvents();
     }
+
+    private void initIcons() {
+        Typeface question = Typeface.createFromAsset(getAssets(), "iconfonts/question_icon.ttf");
+        Typeface interactIcon = Typeface.createFromAsset(getAssets(), "iconfonts/interact_icon.ttf");
+        backIcon.setTypeface(question);
+        commentIcon.setTypeface(interactIcon);
+    }
+    private void initEvents(){
+        backIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+    private void initViews() {
+        backIcon = (TextView) findViewById(R.id.question_detail_back);
+        commentIcon = (TextView) findViewById(R.id.icon_comment);
+    }
+
+
+
 }
